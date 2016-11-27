39b7d76eeab48ba8cc6551834defa046bbc15c7a
xxxrocoxxx <xperiaroco@gmail.com>
2016-11-27 15:11:49 +0200

    RAM

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,9 +12,33 @@ namespace Snake
         {
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
-
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("   p1  ---> " + p1.x + "  " + p1.y);
+            Console.WriteLine("   p2  ---> " + p2.x + "  " + p2.y);
+
+            p1 = p2; // p2 = p1; <-- у автора
+
+            Console.WriteLine(" ");
+            Console.WriteLine("   p1=p2 p1 ---> " + p1.x + "  " + p1.y);
+            Console.WriteLine("   p1=p2 p2 ---> " + p2.x + "  " + p2.y);
+
+            p2.x = 8; // p1.x = 8; для экперемента можно и эти координаты
+            p2.y = 8; // p2.y = 6; меняет координаты обоих Point
+
+            Console.WriteLine(" ");
+            Console.WriteLine("   new p2(8,8) p1 ---> " + p1.x + "  " + p1.y);
+            Console.WriteLine("   new p2(8,8) p2 ---> " + p2.x + "  " + p2.y);
+
+            p1.x = 3; // Не важно какие координаты менять х или у 1-ой или 2-ой Point 
+            p2.y = 5; // меняется всё т.к. адрес тот же
+
+            Console.WriteLine(" ");
+            Console.WriteLine("   new p1.x = 3 p1 ---> " + p1.x + "  " + p1.y);
+            Console.WriteLine("   new p2.y = 5 p2 ---> " + p2.x + "  " + p2.y);
 
             Console.ReadLine();
         }
